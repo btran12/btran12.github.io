38103946be3c78077b3836b64de50b4e3b2f70d4
Bao Tran <baotranomatic@gmail.com>
2015-07-15 09:45:20 -0400

    Hover over sections animation

--- events.js
+++ events.js
@@ -2,6 +2,7 @@ var main = function(){
 	//Hide the mail form when site starts;
 	$('#mailForm').hide();
 
+	//Move it up and down
 	$('#scrollBtn').on({
 		mouseover: function(){
 			$(this).animate({ top: '87%' });
@@ -11,10 +12,12 @@ var main = function(){
 		}
 	});
 
+	//Open up the form
 	$('#sendMailIcon').click(function(){
 		$('#mailForm').slideToggle();
 	});
 
+	//The Send Button
 	$('#sendButton').mouseover(function(){
 		var email = $('#emailField').val();
 		var message = $('#messageField').val();
@@ -23,6 +26,41 @@ var main = function(){
 			alert("The Email, or Message cannot be blank!");
 		}
 	})
+
+	//Mouse over Sections
+	//TODO Make code more efficient
+	$('#aboutSection').on({
+		mouseenter: function(){
+			$('#underliner1').animate({ width: '150px' });
+		},
+		mouseleave: function(){
+			$('#underliner1').animate({ width: '50px' });
+		}
+	});
+	$('#experienceSection').on({
+		mouseenter: function(){
+			$('#underliner2').animate({ width: '150px' });
+		},
+		mouseleave: function(){
+			$('#underliner2').animate({ width: '50px' });
+		}
+	});
+	$('#projectSection').on({
+		mouseenter: function(){
+			$('#underliner3').animate({ width: '150px' });
+		},
+		mouseleave: function(){
+			$('#underliner3').animate({ width: '50px' });
+		}
+	});
+	$('#contactSection').on({
+		mouseenter: function(){
+			$('#underliner4').animate({ width: '150px' });
+		},
+		mouseleave: function(){
+			$('#underliner4').animate({ width: '50px' });
+		}
+	});
 }
 
 $(document).ready(main);
