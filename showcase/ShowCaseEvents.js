e620dade1cbfee0435ebbe48c9a4ea76afc1b568
Bao Tran <baotranomatic@gmail.com>
2015-08-03 12:46:42 -0400

    Styled showcase navigation between projects

--- showcase/ShowCaseEvents.js
+++ showcase/ShowCaseEvents.js
@@ -1,22 +1,44 @@
 var linkToProjects = new Array(
 	"http://baotran.xyz/projects/Escape",
 	"http://baotran.xyz/projects/History-Timeline");
+var projectsDescriptions = new Array(
+	"A basic avoid the enemies game. The game takes the concept of Pacman and simplifies it down to the bare of the bones. \
+	The objective of the game is to stay alive for as long as possible. The player can move around with the arrow keys. \
+	The difficulty of the game increases over time. Good luck.!",
+	"A timeline from 1865 to 1880. Hover over an event to get a better look into the period."
+	);
 
 //Iframe
-var switchTarget$ = $('.switch-project');
+// var switchTarget$ = $('.switch-project');
 
-function switchIframeSrc(target) {
-  // set the 'src' attribute of the iframe
-  switchTarget$.attr('src', target);
-}
+// function switchIframeSrc(target) {
+//   // set the 'src' attribute of the iframe
+//   switchTarget$.attr('src', target);
+// }
 
 var main = function(){
-	$('.EscapeProject').click(function(){
-		switchIframeSrc(linkToProjects[0]);
+	// $('.EscapeProject').click(function(){
+	// 	switchIframeSrc(linkToProjects[0]);
+	// });
+	// $('.TimelineProject').click(function(){
+	// 	switchIframeSrc(linkToProjects[1]);
+	// });
+
+	$(".snapshots").on("mouseover", "img", function(e){
+    	var $target = $(e.currentTarget);
+    	$target.animate({
+    		width: '90px',
+    		height: '90px'
+    	}, 200);
 	});
-	$('.TimelineProject').click(function(){
-		switchIframeSrc(linkToProjects[1]);
+	$(".snapshots").on("mouseout", "img", function(e){
+    	var $target = $(e.currentTarget);
+    	$target.animate({
+    		width: '80px',
+    		height: '80px'
+    	}, 200);
 	});
+
 }
 
 $(document).ready(main);
