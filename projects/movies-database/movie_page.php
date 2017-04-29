459271ece4ac2239df9505506c4a632a709c1d10
Bao Tran <btran@westfield.ma.edu>
2017-04-29 12:40:31 -0400

    Add more sanitizations to users reviews

--- projects/movies-database/movie_page.php
+++ projects/movies-database/movie_page.php
@@ -225,7 +225,7 @@ if (empty($_POST)){
 	//Clean data just in case of injections
 	$username = sanitize($_POST["user_name"]);
 	$userrating = sanitize($_POST["user_rating"]);
-	$userreview = sanitize($_POST["user_review"]);
+	$userreview = preg_replace("/[^a-zA-Z0-9]+/", "", sanitize($_POST["user_review"]));
 	$movieid = sanitize($_POST["movie_id"]);
 
 	//Get the current date
