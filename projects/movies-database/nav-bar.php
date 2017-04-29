3c9ca9f3671a6822bb3c85d74713b35dd687b61c
Bao Tran <btran@westfield.ma.edu>
2017-04-29 13:22:43 -0400

    wip

--- projects/movies-database/nav-bar.php
+++ projects/movies-database/nav-bar.php
@@ -5,10 +5,8 @@
 	if (session_status() == PHP_SESSION_NONE) {
     	session_start();
 	}
-	$admin_page = "";
 	if (!isset($one) && !isset($two) && !isset($three) && !isset($four) && !isset($five) && !isset($six)
 		&& !isset($seven) && !isset($eight) && !isset($nine) && !isset($ten) && !isset($eleven)){
-		$one = $two = $three = $four = $five = $six = $seven = $eight = $nine = $ten = $eleven = "";
 		$admin_page="active";
 	}
 	
@@ -17,58 +15,58 @@
 	<div class='navigation-bar'>
 		<ul>
 			<li>
-				<a href='index.php?page=main' class='".$one."'>
+				<a href='index.php?page=main' class='".isset($one) ? $one : ""."'>
 					Upcoming Movies
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=current' class='".$two."'> 
+				<a href='index.php?page=current' class='".isset($two) ? $two : ""."'> 
 					Currently Showing Movies 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=topall' class='".$three."'> 
+				<a href='index.php?page=topall' class='".isset($three) ? $three : ""."'> 
 					Best Movies of All Times 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top15' class='".$four."'> 
+				<a href='index.php?page=top15' class='".isset($four) ? $four : ""."'> 
 					Top Movies of 2015 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top14' class='".$five."'> 
+				<a href='index.php?page=top14' class='".isset($five) ? $five : ""."'> 
 					Top Movies of 2014 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top13' class='".$six."'> 
+				<a href='index.php?page=top13' class='".isset($six) ? $six : ""."'> 
 					Top Movies of 2013 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top12' class='".$seven."'>
+				<a href='index.php?page=top12' class='".isset($seven) ? $seven : ""."'>
 					Top Movies of 2012 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top11' class='".$eight."'> 
+				<a href='index.php?page=top11' class='".isset($eight) ? $eight : ""."'> 
 					Top Movies of 2011 
 				</a>
 			</li>
 			<li>
-				<a href='index.php?page=top10' class='".$nine."'> 
+				<a href='index.php?page=top10' class='".isset($nine) ? $nine : ""."'> 
 					Top Movies of 2010 
 				</a>
 			</li>
 			<li>
-				<a href='showtimes.php' class='".$eleven."'> 
+				<a href='showtimes.php' class='".isset($eleven) ? $eleven : ""."'> 
 					Showtimes Near You
 				</a>
 			</li>
 
 			<li>
-				<a href='admin_page.php' class='".$admin_page."'> 
+				<a href='admin_page.php' class='".isset($admin_page) ? $admin_page : ""."'> 
 					Administration
 				</a>
 			</li>
@@ -78,7 +76,7 @@
 				</a>
 			</li>
 			<li>
-				<a href='movie_lookup.php' class='".$ten."'> 
+				<a href='movie_lookup.php' class='".isset($ten) ? $ten : ""."'> 
 					Search <img src='https://cdn1.iconfinder.com/data/icons/free-98-icons/32/search-128.png' style='width:16px;height:16px;'>
 				</a>
 			</li>";
