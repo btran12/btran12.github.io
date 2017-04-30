6cc1b2697839e4640896a565246309aec46a0285
btran12 <btran12@users.noreply.github.com>
2017-04-30 17:00:12 -0400

    wip

--- projects/send-mail/payrent.php
+++ projects/send-mail/payrent.php
@@ -3,7 +3,14 @@
 	$to = "baotr1340@gmail.com";
 	$subject = "326 - Please run my card <eom>";
 
-	$message = "";
+	$message = "
+		<html>
+			<body>
+				<h2>This is an automated message</h2>
+				<p>For any questions please reply to this email or call me at (413) 626-3714</p>
+			</body>
+		</html>
+	";
 
 	// Always set content-type when sending HTML email
 	$headers = "MIME-Version: 1.0" . "\r\n";
@@ -11,7 +18,7 @@
 
 	// More headers
 	$headers .= 'From: <btran1340@gmail.com>' . "\r\n";
-	$headers .= 'Cc: me@baotran.xyz' . "\r\n";
+	$headers .= 'Cc: <baotranomatic@gmail.com>' . "\r\n";
 
 	mail($to,$subject,$message,$headers);
 ?>
